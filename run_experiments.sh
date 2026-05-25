d55da6be843f0511455af0059ea607f9a3e71bda
dsms2-ua <dsms2@alu.ua.es>
2026-05-25 15:28:39 +0200

    resultados de ctc

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 metodo="ctc" #Valores posibles: ctc, rnn-t, seq2seq
+encoding="kern" #Valores posibles: decoupled, decoupled-dot, kern
 
-for i in {1..6}; do
-    python3 train.py --method $metodo --experiment_id $i --encoding decoupled --batch_size 16 --epochs 300 --patience 25
+for i in {5..6}; do
+    python3 train.py --method $metodo --experiment_id $i --encoding $encoding --batch_size 8 --epochs 300 --patience 25
 done
